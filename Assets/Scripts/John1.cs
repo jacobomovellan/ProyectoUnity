edf9b0a92f7284250adbbeaf6ca0dbab0b8cf096
jacob <jacobomovellan@liceolapaz.net>
2025-03-13 20:35:02 +0100

    He incluido a John la función de salto

--- Assets/Scripts/John1.cs
+++ Assets/Scripts/John1.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class John1 : MonoBehaviour
 {
+    public float Speed;
+    public float JumpForce;
+
     private Rigidbody2D Rigidbody2D;
     private float Horizontal;
+    private bool Grounded;
 
     void Start()
     {
@@ -14,10 +18,28 @@ public class John1 : MonoBehaviour
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red );
+
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        {
+            Grounded = true;
+        }
+        else Grounded = false;
+
+        if (Input.GetKeyDown(KeyCode.W ) && Grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+    Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
 
     private void FixedUpdate()
     {
-        Rigidbody2D.linearVelocity = new Vector2(Horizontal, Rigidbody2D.linearVelocity.y);
+        Rigidbody2D.linearVelocity = new Vector2(Horizontal * Speed, Rigidbody2D.linearVelocity.y);
     }
 }
