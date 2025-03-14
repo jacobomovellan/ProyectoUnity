532f20c58523241f6692bc6e641b815c2bf9668b
jacob <jacobomovellan@liceolapaz.net>
2025-03-14 09:57:11 +0100

    He añadido animaciones a John, correciones en el movimiento, y la opción de disparar aunque aún no esta corecta del todo

---
+++ Assets/Scripts/BalaScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalaScript : MonoBehaviour
+{
+     public float Speed;
+     private Rigidbody2D Rigidbody2D;
+     private Vector2 Direction;
+
+    void Start()
+    {
+
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.linearVelocity = Direction * Speed;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        Direction = direction;
+    }
+}
--- Assets/Scripts/John1.cs
+++ Assets/Scripts/John1.cs
@@ -2,22 +2,30 @@ using UnityEngine;
 
 public class John1 : MonoBehaviour
 {
+    public GameObject BalaPrefab;
     public float Speed;
     public float JumpForce;
 
     private Rigidbody2D Rigidbody2D;
+    private Animator Animator;
     private float Horizontal;
     private bool Grounded;
 
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f) transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
+        else if (Horizontal > 0.0f) transform.localScale = new Vector3(1.0f,1.0f,1.0f);
+
+        Animator.SetBool("running",Horizontal != 0.0f);
 
         Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red );
 
@@ -31,11 +39,27 @@ public class John1 : MonoBehaviour
         {
             Jump();
         }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Shoot();
+        }
     }
 
     private void Jump()
     {
     Rigidbody2D.AddForce(Vector2.up * JumpForce);
+    }
+
+    private void Shoot()
+    {
+
+        Vector3 direction;
+        if(transform.localScale.x == 1.0f) direction = Vector2.right;
+        else direction = Vector2.left;
+
+        GameObject bala = Instantiate(BalaPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        bala.GetComponent<BalaScript>().SetDirection(direction);
     }
 
     private void FixedUpdate()
